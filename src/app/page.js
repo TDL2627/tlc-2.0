b55bfcf472e5700e0409a4b3d4fb7a56799bb803
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-09-24 12:56:38 +0200

    first upload

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" w-full text-center   grid justify-center items-center h-screen">
+      <div>
+        <h1 className="lg:text-7xl text-3xl font-black">The Learning Centre</h1>
+        <p className=" my-5">..Coming Soon...</p>
+      </div>
+    </main>
+  );
+}
