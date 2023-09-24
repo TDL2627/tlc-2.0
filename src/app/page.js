d589a8b2c2005e00c8d40f18b8ab7a08e500ad44
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-09-24 15:16:12 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className=" w-full text-center   grid justify-center items-center h-screen">
-      <div>
-        <h1 className="lg:text-7xl text-3xl font-black">The Learning Centre</h1>
-        <p className=" my-5">Coming Soon...</p>
+    <main className=" w-full text-center   grid content-between h-screen justify-center">
+      <div className="mt-64 ">
+        <h1 className="lg:text-8xl text-3xl font-black">The Learning Centre</h1>
+        <p className="lg:text-2xl my-5">Coming Soon...</p>
       </div>
+      <Image src="/kid.png" width={1000} height={1000} />
     </main>
   );
 }
