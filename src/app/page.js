d31b905d4c5d3926790520bfe95a7eb0a5e58469
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-09-24 14:51:26 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main className=" w-full text-center   grid justify-center items-center h-screen">
       <div>
         <h1 className="lg:text-7xl text-3xl font-black">The Learning Centre</h1>
-        <p className=" my-5">..Coming Soon...</p>
+        <p className=" my-5">Coming Soon...</p>
       </div>
     </main>
   );
